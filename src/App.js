73702f1ca269c367405645983072a85f292a2893
Dino Tudor <dinotudor@gmail.com>
2019-07-22 12:45:38 +0200

    remove unsed files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-//import Add from './components/Add'
-import AddUser from './components/AddUser'
-import Add2 from './components/Add2'
-
-
 
 function App() {
   return (
@@ -15,11 +10,9 @@ function App() {
     <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/AddUser" component={AddUser} />
-        <Route path="/Add2" component={Add2} />
       </Switch>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div>
-      <nav>
-        <ul>
-          <Link to="/">Home</Link>
-        </ul>
-      </nav>
+      <h1>User Manager</h1>
     </div>
   )
 }
